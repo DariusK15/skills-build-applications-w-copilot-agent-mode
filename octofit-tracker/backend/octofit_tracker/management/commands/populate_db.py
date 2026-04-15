f79a1bfcb4f50eb8f6532cbaf970b6bb1b218677
DariusK15 <darius.kiminius@accenture.com>
2026-04-15 08:20:52 +0000

    Add REST API and prompt for OctoFit Tracker backend

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,75 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from djongo import models
+
+from django.db import connection
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Activity(models.Model):
+    user = models.CharField(max_length=100)
+    activity_type = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    team = models.CharField(max_length=100)
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Leaderboard(models.Model):
+    team = models.CharField(max_length=100)
+    points = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    difficulty = models.CharField(max_length=50)
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        # Delete all data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        users = [
+            User.objects.create_user(username='ironman', email='ironman@marvel.com', password='password', first_name='Tony', last_name='Stark'),
+            User.objects.create_user(username='captainamerica', email='cap@marvel.com', password='password', first_name='Steve', last_name='Rogers'),
+            User.objects.create_user(username='batman', email='batman@dc.com', password='password', first_name='Bruce', last_name='Wayne'),
+            User.objects.create_user(username='wonderwoman', email='wonderwoman@dc.com', password='password', first_name='Diana', last_name='Prince'),
+        ]
+
+        # Create activities
+        Activity.objects.create(user='ironman', activity_type='Running', duration=30, team='Marvel')
+        Activity.objects.create(user='captainamerica', activity_type='Cycling', duration=45, team='Marvel')
+        Activity.objects.create(user='batman', activity_type='Swimming', duration=25, team='DC')
+        Activity.objects.create(user='wonderwoman', activity_type='Yoga', duration=60, team='DC')
+
+        # Create leaderboard
+        Leaderboard.objects.create(team='Marvel', points=75)
+        Leaderboard.objects.create(team='DC', points=85)
+
+        # Create workouts
+        Workout.objects.create(name='Super Strength', description='Strength training for superheroes', difficulty='Hard')
+        Workout.objects.create(name='Agility Boost', description='Agility and flexibility exercises', difficulty='Medium')
+
+
+        # Note: Djongo does not support direct MongoDB index creation via SQL. Please create the unique index on email using mongosh:
+        # mongosh --eval 'db.users.createIndex({ "email": 1 }, { unique: true })' octofit_db
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data.'))
